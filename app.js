39e7a1f3523ffba1629bb8bdacbe64f99b56ee18
Ladina Bilgery <ladina.bilgery@ntb.ch>
2017-10-25 13:47:56 +0200

    Taks hinzufügen und Checkbox anzeigen

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var app = express();
+var express  	= require('express');
+var path 		= require('path');
+var bodyParser 	= require('body-parser');
+var app 		= express();
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// create application/json parser
-var jsonParser = bodyParser.json()
+var jsonParser 	= bodyParser.json()
 
 var tasks = [
 	{
-	taskid: 1,
-	task: "initial Task",
-	checked: false
+	taskid: 	1,
+	task: 		"initial Task",
+	checked: 	true
 	}
 ];
 var task = "";
@@ -26,15 +25,6 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/todo.html'));
 });
 
-// app.get('/items', function (req, res) {
-//   res.send('Das sind viele Items');
-// });
-
-// app.get('/create?:pendenz', function(req, res) {
-// 	//res.send('Das sind create Items');
-//   	res.send('Add: ' + req.query.pendenz);
-// });
-
 app.post('/create', function(req, res) {
 	taskimport = req.body.pendenz;
 	task = {
@@ -42,17 +32,38 @@ app.post('/create', function(req, res) {
 		task: taskimport,
 		checked: false
 	};
+	id++;
 	tasks.push(task);
-	res.redirect('/listTasks').sendStatus(200);
-
-})
+	res.json(JSON.stringify(tasks));
+});
 
 app.get('/listTasks', function(req, res) {
-	var json = JSON.stringify(tasks);
-	res.json(json);
-})
+	res.json(JSON.stringify(tasks));
+});
+
+
+app.get('/listClosedTasks', function(req, res) {
+	var closedTasks = tasks.filter(function(taskItem){
+		if (taskItem.checked == true){
+			return true;
+		}
+		return false;
+	});
+	res.json(JSON.stringify(closedTasks));
+});
+
+
+app.get('/listOpenTasks', function(req, res) {
+	var openTasks = tasks.filter(function(taskItem){
+		if (taskItem.checked == false){
+			return true;
+		}
+		return false;
+	});
+	res.json(JSON.stringify(openTasks));
+});
 
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('App listening on port 3000!');
 });
